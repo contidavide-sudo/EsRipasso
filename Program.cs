20a714c4650f9270fe5e27f0d8c53171711b6e76
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-09-15 09:09:59 +0200

    proseguimento esercizi ripasso

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
     internal class Program
        
     {
+
+        //es 1
         static void Main(string[] args)
         {  
 
@@ -31,6 +33,9 @@
                 Console.WriteLine("dispari");
             }
 
+            Console.WriteLine("-----------------------------------------");
+
+            //es 2
 
             Console.Write("Inserisci un numro tra 1 e 10: ");
             num=int.Parse(Console.ReadLine());
@@ -42,7 +47,86 @@
                 tab = tab + num;
                 Console.WriteLine(tab);
             }
-                    
+
+            Console.WriteLine("-----------------------------------------");
+
+            //es 3
+
+            int n, m, x = 2, numM, cont = 0;
+
+            Console.Write("Inserisci n: ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("Inserisci m: ");    
+            m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (n < m)
+            {
+                numM = n;
+            }
+            else
+            {
+                numM = m;
+            }
+
+            while(x <= numM)
+            {
+                if(n % x == 0 && m % x == 0)
+                {
+                    cont = cont + 1;
+                }
+
+                x++;
+            }
+
+            Console.Write(cont);
+
+            Console.WriteLine("-----------------------------------------");
+
+            //es 4
+
+            int voti;
+
+            double votoSin, somma = 0;
+
+            Console.Write("Qunati voti vuoi inserire: ");
+            voti = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci voto: ");
+
+            for(int i = 0;i < voti; i++)
+            {
+                votoSin = 0;
+
+                votoSin = int.Parse(Console.ReadLine());
+
+                somma = somma + votoSin;
+            }
+
+            double media = somma / voti;
+
+            Console.WriteLine();
+
+            Console.WriteLine(media);
+
+            Console.WriteLine("-----------------------------------------");
+
+            //es 5 
+
+            int b = 5, h = 4;
+
+            for(int i = 0; i < h; i++)
+            {
+                for(int z = 0; z < b; z++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
